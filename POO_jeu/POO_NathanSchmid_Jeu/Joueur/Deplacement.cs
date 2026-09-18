7485d2fddd6b9000505d9a562e8bb3bdde2328dd
ɴᴀᴛʜᴀɴ <px61pje@eduvaud.ch>
2026-09-18 15:31:46 +0200

    feat(newClass): ajout de plein de classes d'ont GameSpace.cs, personnages.cs, config.cs et deplacement.cs

---
+++ POO_jeu/POO_NathanSchmid_Jeu/Joueur/Deplacement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_NathanSchmid_Jeu.Joueur
+{
+    internal class Deplacement
+    {
+        public float posX = 0;
+        public char keyChar { get; set; }
+
+        public Deplacement(float posX)
+        {
+            this.posX = posX;
+        }
+        
+        public void Update()
+        {
+            
+        }
+
+    }
+}
---
+++ POO_jeu/POO_NathanSchmid_Jeu/config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_NathanSchmid_Jeu
+{
+    internal class config
+    {
+        public const int SPEED = 5;
+
+        public const int GAMESPACE_WIDTH = 1000;
+        public const int GAMESPACE_HEIGHT = 500;
+
+    }
+}
